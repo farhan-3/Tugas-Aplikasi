eee63583f4adf3ff73bdf819d68d8323cb5f5e3a
Muhammad Farhan <157960532+farhan-3@users.noreply.github.com>
2025-05-30 09:16:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -132,3 +132,12 @@ document.addEventListener('DOMContentLoaded', function() {
       return true;
   }
 });
+
+document.getElementById('confirmasi1').checked = true;
+document.getElementById('confirmasi2').checked = false;
+document.getElementById('confirmasi3').checked = false;
+document.getElementById('confirmasi4').checked = false;
+document.getElementById('confirmasi5').checked = false;
+document.getElementById('confirmasi6').checked = false;
+document.getElementById('confirmasi7').checked = false;
+document.getElementById('confirmasi8').checked = true;
