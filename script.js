bbb6ea55bd32acbfc2a5e3dc0babf3cec333a431
Muhammad Farhan <157960532+farhan-3@users.noreply.github.com>
2025-05-30 09:19:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -133,11 +133,11 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-document.getElementById('confirmasi1').checked = true;
-document.getElementById('confirmasi2').checked = false;
-document.getElementById('confirmasi3').checked = false;
-document.getElementById('confirmasi4').checked = false;
-document.getElementById('confirmasi5').checked = false;
-document.getElementById('confirmasi6').checked = false;
-document.getElementById('confirmasi7').checked = false;
-document.getElementById('confirmasi8').checked = true;
+// document.getElementById('confirmasi1').checked = true;
+// document.getElementById('confirmasi2').checked = false;
+// document.getElementById('confirmasi3').checked = false;
+// document.getElementById('confirmasi4').checked = false;
+// document.getElementById('confirmasi5').checked = false;
+// document.getElementById('confirmasi6').checked = false;
+// document.getElementById('confirmasi7').checked = false;
+// document.getElementById('confirmasi8').checked = true;
