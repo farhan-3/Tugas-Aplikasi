ca36082ad6da1327efe3a97c68612da1506524cb
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2025-06-17 21:59:58 +0700

    Delete script3.js

--- script3.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('confirmasi1').checked = true;
-document.getElementById('confirmasi2').checked = false;
-document.getElementById('confirmasi3').checked = false;
-document.getElementById('confirmasi4').checked = false;
-document.getElementById('confirmasi5').checked = false;
-document.getElementById('confirmasi6').checked = false;
-document.getElementById('confirmasi7').checked = false;
-document.getElementById('confirmasi8').checked = true;
